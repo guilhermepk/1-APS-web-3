fd87e6046cea0260c21c674dccf63c0ddc7d18cb
Guilherme Prigol Kramer <guilhermeprigolkramer@gmail.com>
2025-05-13 19:51:09 -0300

    correções e modulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
+import { ProjectsModule } from './projects/projects.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ProjectsModule, TasksModule],
 })
 export class AppModule { }
